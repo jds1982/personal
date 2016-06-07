629b82a656b43b8eeb0de351ce90be53bc0df15f
jds1982 <jds1982@hotmail.com>
2016-06-07 16:34:52 -0400

    commit

---
+++ Picture Viewer/ViewerForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Picture_Viewer
+{
+    partial class ViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectPicture = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.picShowPicture = new System.Windows.Forms.PictureBox();
+            this.ofdSelectPicture = new System.Windows.Forms.OpenFileDialog();
+            this.btnEnlarge = new System.Windows.Forms.Button();
+            this.btnShrink = new System.Windows.Forms.Button();
+            this.btnDrawBorder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picShowPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectPicture
+            // 
+            this.btnSelectPicture.Location = new System.Drawing.Point(295, 10);
+            this.btnSelectPicture.Name = "btnSelectPicture";
+            this.btnSelectPicture.Size = new System.Drawing.Size(85, 23);
+            this.btnSelectPicture.TabIndex = 0;
+            this.btnSelectPicture.Text = "Select Picture";
+            this.btnSelectPicture.UseVisualStyleBackColor = true;
+            this.btnSelectPicture.Click += new System.EventHandler(this.btnSelectPicture_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(295, 40);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(85, 23);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // picShowPicture
+            // 
+            this.picShowPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picShowPicture.Location = new System.Drawing.Point(8, 8);
+            this.picShowPicture.Name = "picShowPicture";
+            this.picShowPicture.Size = new System.Drawing.Size(282, 275);
+            this.picShowPicture.TabIndex = 2;
+            this.picShowPicture.TabStop = false;
+            this.picShowPicture.Click += new System.EventHandler(this.picShowPicture_Click);
+            // 
+            // ofdSelectPicture
+            // 
+            this.ofdSelectPicture.Filter = "Windows Bitmaps|*.BMP|JPEG Files|*.JPG|GIF File|*.GIF";
+            this.ofdSelectPicture.Title = "Select Picture";
+            this.ofdSelectPicture.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdSelectPicture_FileOk);
+            // 
+            // btnEnlarge
+            // 
+            this.btnEnlarge.Location = new System.Drawing.Point(338, 261);
+            this.btnEnlarge.Name = "btnEnlarge";
+            this.btnEnlarge.Size = new System.Drawing.Size(21, 23);
+            this.btnEnlarge.TabIndex = 3;
+            this.btnEnlarge.Text = "^";
+            this.btnEnlarge.UseVisualStyleBackColor = true;
+            this.btnEnlarge.Click += new System.EventHandler(this.btnEnlarge_Click);
+            // 
+            // btnShrink
+            // 
+            this.btnShrink.Location = new System.Drawing.Point(365, 261);
+            this.btnShrink.Name = "btnShrink";
+            this.btnShrink.Size = new System.Drawing.Size(21, 23);
+            this.btnShrink.TabIndex = 4;
+            this.btnShrink.Text = "v";
+            this.btnShrink.UseVisualStyleBackColor = true;
+            this.btnShrink.Click += new System.EventHandler(this.btnShrink_Click);
+            // 
+            // btnDrawBorder
+            // 
+            this.btnDrawBorder.Location = new System.Drawing.Point(295, 69);
+            this.btnDrawBorder.Name = "btnDrawBorder";
+            this.btnDrawBorder.Size = new System.Drawing.Size(85, 23);
+            this.btnDrawBorder.TabIndex = 5;
+            this.btnDrawBorder.Text = "Draw Border";
+            this.btnDrawBorder.UseVisualStyleBackColor = true;
+            this.btnDrawBorder.Click += new System.EventHandler(this.btnDrawBorder_Click);
+            // 
+            // ViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 287);
+            this.Controls.Add(this.btnDrawBorder);
+            this.Controls.Add(this.btnShrink);
+            this.Controls.Add(this.btnEnlarge);
+            this.Controls.Add(this.picShowPicture);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnSelectPicture);
+            this.Name = "ViewerForm";
+            this.Text = "Picture Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.picShowPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectPicture;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.PictureBox picShowPicture;
+        private System.Windows.Forms.OpenFileDialog ofdSelectPicture;
+        private System.Windows.Forms.Button btnEnlarge;
+        private System.Windows.Forms.Button btnShrink;
+        private System.Windows.Forms.Button btnDrawBorder;
+    }
+}
+
---
+++ Picture Viewer/ViewerForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Viewer
+{
+    public partial class ViewerForm : Form
+    {
+        public ViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectPicture_Click(object sender, EventArgs e)
+        {
+            //Show the open file dialog box.
+            if (ofdSelectPicture.ShowDialog()==DialogResult.OK)
+            {
+                //Load the picture into the box.
+                picShowPicture.Image = Image.FromFile(ofdSelectPicture.FileName);
+                //Show the name of the file in the forms caption
+                this.Text = string.Concat("Picture Viewer(" + ofdSelectPicture.FileName + ")");
+
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            //Close the window and exit the application.
+            this.Close();
+
+        }
+
+        private void ofdSelectPicture_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void picShowPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnlarge_Click(object sender, EventArgs e)
+        {
+            this.Width = this.Width + 20;
+            this.Height = this.Height + 20;
+
+        }
+
+
+        private void btnShrink_Click(object sender, EventArgs e)
+        {
+            this.Width = this.Width - 20;
+            this.Height = this.Height - 20;
+        }
+
+        private void btnDrawBorder_Click(object sender, EventArgs e)
+        {
+            Graphics objGraphics = null;
+            objGraphics = this.CreateGraphics();
+            objGraphics.Clear(SystemColors.Control);
+            objGraphics.DrawRectangle(Pens.Blue,
+                picShowPicture.Left-1,picShowPicture.Top-1,
+                picShowPicture.Width+1,picShowPicture.Height+1);
+            objGraphics.Dispose();
+
+
+        }
+
+
+    }
+}
